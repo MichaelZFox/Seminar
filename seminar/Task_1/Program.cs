b22b070d9919241faa59c16f2233af3a94d7232b
«Mike <fox9403@gmail.com>
2022-08-29 14:29:20 +0300

    Проверка двух чисел не является ли одно из них квадратом другого

---
+++ seminar/Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+
+Console.Write("Enter two integer numbers ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+int min = numB;
+
+if(numA > max)
+{ max = numA;
+  min = numB;
+}
+if(numB > max) 
+{
+    max = numB;
+    min = numA;
+}
+
+if(min * min == max)
+{
+    Console.WriteLine($"Число {min} является квадратом числа {max}");
+}
+
+if(min * min != max)
+{
+    Console.WriteLine($"Число{min} не является квадратом числа {max}");
+}
