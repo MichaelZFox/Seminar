d1354b1a851363c1999e1c6c44e5dcc72cd570cb
«Mike <fox9403@gmail.com>
2022-08-29 17:03:46 +0300

    add task 3 need fix

---
+++ seminar/Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+if(num > 0)
+{
+i = num;
+num = num * -1;
+}
+else
+{
+    i = -num;
+}
+
+while (i<=num)
+{
+    Console.Write($"{i}");
+    i++;
+}
