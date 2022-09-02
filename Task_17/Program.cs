8d54aea7d02f782a8ff25a51a754d7131c07b42c
«Mike <fox9403@gmail.com>
2022-09-02 11:35:08 +0300

    fix code Task_17

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -14,10 +14,10 @@ int y = Convert.ToInt32(Console.ReadLine());
 
 string Quarter(int xc, int yc)
 {
-if (x > 0 && y > 0) return "Первая четверть";
-if (x < 0 && y > 0) return "Вторая четверть";
-if (x < 0 && y < 0) return "Третья четверть";
-if (x > 0 && y < 0) return "Четвертая четверть";
+if (xc > 0 && yc > 0) return "Первая четверть";
+if (xc < 0 && yc > 0) return "Вторая четверть";
+if (xc < 0 && yc < 0) return "Третья четверть";
+if (xc > 0 && yc < 0) return "Четвертая четверть";
 return "Введены некорректные данные";
 }
 string res = Quarter(x, y);
