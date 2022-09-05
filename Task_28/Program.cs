c2d5e80cd66b6921d402c7bde139084788792974
«Mike <fox9403@gmail.com>
2022-09-05 18:11:39 +0300

    added second branch on task 28

---
+++ Task_28/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, 
+// которая принимает на вход число N 
+// и выдает произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+double number = Convert.ToInt32(Console.ReadLine());
+
