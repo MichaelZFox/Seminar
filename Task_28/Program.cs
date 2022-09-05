55d9c184c5777f1ac84b48f465cb9baac86cbce7
«Mike <fox9403@gmail.com>
2022-09-05 18:09:08 +0300

    added Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, 
+// которая принимает на вход число N 
+// и выдает произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+double number = Convert.ToInt32(Console.ReadLine());
+
+double GetDigit(double num)
+{
+    double count = 1;
+    for (double i = 1; i <= num; i++)
+    {
+        count = count * i;
+    }
+    
+    return count;
+}
+
+double result = GetDigit(number);
+Console.WriteLine(result);
