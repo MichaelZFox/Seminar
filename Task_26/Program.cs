ba5675d0714d3b2d758ca639ca4a40ab407c3a44
«Mike <fox9403@gmail.com>
2022-09-05 17:17:55 +0300

    added all

---
+++ Task_26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 26. Напишите программу, 
+// которая принимает на вход число 
+// и выдает колличество цифр в числе
+// 456 -> 3;
+// 78 -> 2;
+// 89126 -> 5;
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int GetDigitCount(int num)
+{
+    int res = 0;
+    while (num >= 1)
+    {
+        num = num / 10;
+        res++;
+    }
+    return res;
+}
+int result = GetDigitCount(number);
+Console.WriteLine($"Количество цифр в числе {number} равняется {result}");
+
