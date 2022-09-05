92dbc9835de9a0d9f4553ddf73ff1e0ea3f07f9e
«Mike <fox9403@gmail.com>
2022-09-05 17:53:10 +0300

    added task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindCountDigit(int num)
+{
+    int index = 1;
+    int res = 0;
+    while (index <= num)
+    {
+        res = res + 1;
+        index = index * 10;
+    }
+    return res;
+}
+
+int result = FindCountDigit(number);
+Console.WriteLine(result);
