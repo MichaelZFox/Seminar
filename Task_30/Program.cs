24754d8aa9fc31edd1c9fffc72cb25ecd000df65
«Mike <fox9403@gmail.com>
2022-09-06 09:38:46 +0300

    Added task_30

--- Task_30/Program.cs
+++ Task_30/Program.cs
@@ -1,28 +1,47 @@
 ﻿Console.Clear();
+
 void FillArray(int[] collection)
 {
     int lenght = collection.Length;
     int index = 0;
     while(index < lenght)
     {
-        collection[index] = new Random().Next(0, 1);
+        collection[index] = new Random().Next(0, 2);
+        //index = index + 1;
         index++;
     }
 }
+
 void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
-    while(position < 0)
+    while(position < count)
     {
-        Console.WriteLine(col[position]);
+        Console.Write(col[position]);
         position++;
     }
 }
 
-int[] array = new int[10];
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int [8]; //new int[8] - создан новый массив в котором будет 8 эллементов 
+
 FillArray(array);
-array[4] = 0;
-array[6] = 1;
 PrintArray(array);
 Console.WriteLine();
